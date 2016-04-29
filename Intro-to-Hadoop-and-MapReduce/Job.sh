1f5012f0144c41eaf717bfd0fd9ba95e6345b784
Ogaga Uzoh <friendsofglory@gmail.com>
2016-04-29 08:35:23 -0400

    Update Job.sh

--- Intro-to-Hadoop-and-MapReduce/Job.sh
+++ Intro-to-Hadoop-and-MapReduce/Job.sh
@@ -4,6 +4,5 @@ head -50 ../data/purchases.txt > testfile.txt
 #test mappy.py
 cat testfile.txt | ./mappy.py
 
-
 #simulate entire process for the first 50 lines
 cat testfile.txt | ./mapper.py | sort | ./reducer.py
