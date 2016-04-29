032d04aabaf4bb1a9f6b4170b984a3ccd0090da1
Ogaga Uzoh <friendsofglory@gmail.com>
2016-04-29 08:35:53 -0400

    Create reducer.py

---
+++ Intro-to-Hadoop-and-MapReduce/reducer.py
@@ -0,0 +1,24 @@
+def reducer():
+
+	salesTotal = 0
+	oldKey = None
+
+	for line in sys.stdin:
+		data = line.strip().split("\t")
+
+		if len(data) !=2:
+			continue
+
+		thisKey, thisSale = data
+
+		if oldKey and oldKey != thisKey:
+			print "{0}\t{1}".format(oldKey, salesTotal)
+
+			salesTotal = 0
+
+		oldKey = thisKey
+		salesTotal += float(thisSale)   #sum of sales
+
+	#
+	if oldKey !=None:
+		print "{0}\t{1}".format(oldKey, salesTotal)
