5fae575c4e4765d261de1e9bc308167724588a49
Ogaga Uzoh <friendsofglory@gmail.com>
2016-05-19 15:27:19 -0400

    Create svm_author_id.py

---
+++ Intro-to-Machine-Learning/svm_author_id.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+
+
+
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+
+#########################################################
+### we handle the import statement and SVC creation for you here
+from sklearn.svm import SVC
+clf = SVC(kernel="linear")
+
+
+#### now your job is to fit the classifier
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+#### using the training features/labels, and to
+#### make a set of predictions on the test data
+t0 = time() # timing your NB Classifier
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s" #duration the NB Classifier takes
+
+#### store your predictions in a list named pred
+t0 = time() # timing of prediction
+pred=clf.predict(features_test)
+#print pred
+print "predicting time:", round(time()-t0, 3), "s" #duration the NB Classifier takes
+
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(labels_test, pred)
+
+print acc
+
+def submitAccuracy():
+    return acc
+
