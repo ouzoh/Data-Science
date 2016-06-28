49583ffb19441c61814dc228271b2c47a0be429f
Ogaga Uzoh <friendsofglory@gmail.com>
2016-06-28 14:43:34 -0400

    Update 01_defineFunction.R

--- R/01_defineFunction.R
+++ R/01_defineFunction.R
@@ -7,3 +7,33 @@ Example
 add <- function(x, y=1) {
   x+y
 }
+
+###########################
+3 components to functions
+###########################
+1. formals argument(add)
+$x
+
+$y
+[1] 1
+
+2. body(add)
+{
+  x+y
+}
+
+3. environment(add)
+<environment: R_GlobalEnv>
+
+###################
+Return value: below code returns absolute value
+###################
+f <- function(x) {
+  if (x < 0) {
+    -x
+  } else  {
+    x
+  }
+}
+
+Functions can be treated as regular objects
